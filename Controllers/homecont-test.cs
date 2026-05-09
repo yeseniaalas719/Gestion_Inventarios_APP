d12051a40b0c0fb36cd33ceb846c45e9ea9d2db1
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-09 15:29:04 -0600

    fix: validar que la cantidad sea obligatoria y positiva

---
+++ Controllers/homecont-test.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Gestion_Inventarios_APP.Models;
+
+namespace Gestion_Inventarios_APP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DbContexto db = new DbContexto();
+
+        public ActionResult Index()
+        {
+            var listaProductos = db.Productos.ToList();
+            return View(listaProductos);
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(string nombre, int? cantidad)
+        {
+            // VALIDACIÓ NOMBRE
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                ModelState.AddModelError("nombre", "El nombre es obligatorio.");
+            }
+
+            // VALIDACIÓ CANTIDAD Y NÚMERO POSITIVO
+            if (!cantidad.HasValue)
+            {
+                ModelState.AddModelError("cantidad", "La cantidad es obligatoria.");
+            }
+            else if (cantidad <= 0)
+            {
+                ModelState.AddModelError("cantidad", "Debe ingresar un número positivo.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var listaProductos = db.Productos.ToList();
+                return View("Index", listaProductos);
+            }
+
+            var nuevoProducto = new Producto
+            {
+                Nombre = nombre,
+                Cantidad = cantidad.Value
+            };
+
+            db.Productos.Add(nuevoProducto);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
