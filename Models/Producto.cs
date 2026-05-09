017f1f87f3df57f9763fe50fc2dbbbe063c66e34
yeseniaalas719 <yeseniaalas719@gmail.com>
2026-05-08 21:38:05 -0600

    Agregado el modelo Producto y actualización del proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Gestion_Inventarios_APP.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+    }
+
+    public class DbContexto : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
